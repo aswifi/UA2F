3589752b380f316a6a68f1663bc608c758b5ed86
KCYLion <1095756998@qq.com>
2021-08-31 10:13:19 +0800

    Update ua2f.c

--- src/ua2f.c
+++ src/ua2f.c
@@ -443,7 +443,7 @@ int main(int argc, char *argv[]) {
 
     str = malloc(sizeof_buf);
     memset(str, '1', sizeof_buf); // 原始UA参数
-    memcpy(str,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",115); // 自定义UA参数
+    memcpy(str,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",115); // 自定义UA参数
 
     nlh = nfq_nlmsg_put(buf, NFQNL_MSG_CONFIG, queue_number);
     nfq_nlmsg_cfg_put_cmd(nlh, AF_INET, NFQNL_CFG_CMD_BIND);
